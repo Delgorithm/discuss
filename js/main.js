000d6f758f2d474adf65e542fcf33aca9f806d2d
Delgorithm <delgorithm@gmail.com>
2023-05-25 21:30:40 +0200

    js : hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const menu_btn = document.querySelector('.hamburger');
-const mobile_menu = document.querySelector('.navbar_mobile');
+const mobile_menu = document.querySelector('.navbar__mobile');
+const navbar_title = document.querySelector('.navbar')
 
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
+    navbar_title.style.color = "white";
 });
