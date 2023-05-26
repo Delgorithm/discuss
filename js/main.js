8591bfdde7833f85c404b198ce8831f124975033
Delgorithm <delgorithm@gmail.com>
2023-05-26 12:22:07 +0200

    js : hamburger menu + title color

--- js/main.js
+++ js/main.js
@@ -1,9 +1,24 @@
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.navbar__mobile');
-const navbar_title = document.querySelector('.navbar')
+const discuss_title = document.querySelector('.navbar-logo-title');
+
+// btn create account
+const btn_create_account = document.querySelector('.btn__create-account');
+
+let isChanged = false;
 
 menu_btn.addEventListener('click', function () {
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
-    navbar_title.style.color = "white";
+
+    if(!isChanged) {
+        discuss_title.style.color = "white";
+    } else {
+        discuss_title.style.color = "black";
+    }
+    isChanged = !isChanged;
 });
+
+btn_create_account.addEventListener('click', function () {
+    window.location.href = "account.html";
+})
