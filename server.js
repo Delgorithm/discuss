2ebbcac156e4fdab0d365aafcecd6d5b519460f6
Delgorithm <delgorithm@gmail.com>
2023-05-30 00:40:21 +0200

    node js

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = 3000;
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-    app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
-} else {
-    app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, 'index.html'));
-    });
-}
-
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 
-// Stop the server when needed
-function stopServer() {
-    Server.close(() => {
-        console.log("Le serveur est arrêté");
-        process.exit(0);
-    });
-}
-
-process.stdin.resume();
-process.on('SIGINT', stopServer);
-process.on('SIGTERM', stopServer);
+// Ecouter le port 
+app.listen(PORT, () => console.log(`Server listening on port: http://localhost:${PORT}`));
