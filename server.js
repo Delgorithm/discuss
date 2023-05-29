4a8313051c601fb138d3492ae728e95e1b97919f
Delgorithm <delgorithm@gmail.com>
2023-05-29 20:45:23 +0200

    server.js for node.js : Having some real difficulties to add all the CSS and JS files

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello ZAAAWorld!');
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-    console.log(`Si suuuuuuuuuu ${port}`);
-});
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
+} else {
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    });
+}
 
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+
+// Stop the server when needed
 function stopServer() {
     Server.close(() => {
         console.log("Le serveur est arrêté");
