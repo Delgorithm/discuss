1e7371fdcaa5af5e49b8ab31e7fb3492cac86ef6
Delgorithm <delgorithm@gmail.com>
2023-06-06 12:44:47 +0200

    js : account

--- public/js/account.js
+++ public/js/account.js
@@ -3,3 +3,6 @@ const btn_cross = document.querySelector('.cross-exit');
 btn_cross.addEventListener('click', function () {
     window.location.href = "/";
 });
+
+// By clicking on "Sign In" while Pseudonym and passwords are fill, it will redirect the user to its profil
+const btn_signin = document.querySelector('.signin-btn')
