d4e1467957644835d4d6da6479e213a2f45ee893
Delgorithm <delgorithm@gmail.com>
2023-06-06 12:45:00 +0200

    js : server

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+
+
+const upload = multer();
 const app = express();
-const PORT = 3000;
+
+
+const PORT = process.env.PORT || 3000;
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+
+// ------------ START : INSCRIPTION FORM V1 ---------------- //
+
+app.get('/html/account.html', (req, res) => {
+    res.render('form');
+});
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(upload.array());
+app.use(express.static('public'));
+
+app.post('/html/account.html', (req, res) => {
+    const pseudo = req.body.pseudonym;
+    const password = req.body.password;
+    res.sendFile(path.join(__dirname, 'public/html/profil.html'));
+});
+
+// ------------ END : INSCRIPTION FORM V1 ---------------- //
+
 
 // Ecouter le port
 app.listen(PORT, () => console.log(`Server listening on port: http://localhost:${PORT}`));
